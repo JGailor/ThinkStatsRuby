c6277b96a11cbb0c1b34451f36b2abd15fdb40a2
Jeremy Gailor <jeremy@infinitecube.com>
2011-12-06 14:29:21 -0800

    PMF file, tried to remain true to the python source, but the translation doesn't map 1:1

--- pmf.rb
+++ pmf.rb
@@ -3,7 +3,12 @@ module ThinkStats
     attr_reader :hash, :name
     
     def initialize(hash = nil, name = '')
-      @hash = hash || {}
+      if hash.kind_of?(Array)
+        @hash = hash.inject(Hash.new {|h,k| h[k] = 0}) {|hash, value| hash[value] += 1}
+      else
+        @hash = hash || {}
+      end
+      
       @name = name
     end
     
@@ -111,5 +116,54 @@ module ThinkStats
       mu = mu || self.mean
       @hash.inject(0) {|acc, (val, prob)| acc += (prob * (val - mu)**2)}
     end
+  end
+  
+  # These helpers are here because they are in the python source... I don't get the need for them, but
+  # for completeness sake, here you go.
+  def make_hist_from_list(list, name = '')
+    Histogram.new(list, name)
+  end
+  
+  def make_hist_from_dict(dict, name = '')
+    Histogram.new(dict, name)
+  end
+  
+  def make_pmf_from_histogram(histogram, name = '')
+    name = name || histogram.name
+    Pmf.new(histogram.hash.clone, name).tap |pmf|
+      pmf.normalize
+    end
+  end
+  
+  def make_pmf_from_list(list, name = '')
+    make_pmf_from_histogram(make_histogram_from_list(list, name))
+  end
+  
+  def make_pmf_from_hash(hash, name = '')
+    Pmf.new(hash, name).tap do |pmf|
+      pmf.normalize
+    end
+  end
+  
+  # I have no idea what a CDF is at this point, hoping it respects the interface
+  def make_pmf_from_cdf(cdf, name = '')
+    name = name || cdf.name
+    Pmf.new({}, name).tap do |pmf|
+      prev = 0.0
+      cdf.each do |val, prob|
+        pmf.incr(val, prob - prev)
+        prev = prob
+      end
+    end
+  end
+  
+  def make_mixture(pmfs, name = 'mix')
+    Pmf.new({}, name).tap do |mix|
+      pmfs.each do |pmf, prob|
+        pmf.hash.each do |x, p|
+          mix.incr(x, p * prob)
+        end
+      end
+    end 
   end
 end
